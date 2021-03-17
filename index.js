a5590ddb299ef2dbea5f956763fee89bbfe2f4c2
Francisco Tejada <franjavi06@gmail.com>
2021-03-16 23:45:12 -0400

    errorsandvalidation

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
-const moviesApi = require('./routes/movies')
+const moviesApi = require('./routes/movies');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
+const {logErrors, errorHandler, wrapErrors} = require('./utils/middleware/errorHandlers');
 
+//body parser
+app.use(express.json());
+
+//routes
 moviesApi(app);
+
+//Catch 404
+app.use(notFoundHandler);
+
+//Errors middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function(){
     console.log(`Listening http://localhost:${config.port}`);
